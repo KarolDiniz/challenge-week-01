d04245fb587c155d9c0738afe3b30dd39b205a7e
KarolDiniz <dinizkaroline923@gmail.com>
2023-05-09 14:54:13 -0300

    Retificação Main: aderindo aos utilitarios

--- ChallengeWeek01/employeeBonusValidator/run/Main.java
+++ ChallengeWeek01/employeeBonusValidator/run/Main.java
@@ -1,39 +1,32 @@
 package run;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import controller.EmployeeController;
+import utilities.*;
 
 public class Main {
 	
 	public static void main(String[] args) {		
-		Scanner input = new Scanner(System.in);
-		Scanner inputEmployee = new Scanner(System.in);
+		Scanner input = new Scanner(System.in); 
 		EmployeeController employeeController = new EmployeeController();
 		
-		System.out.println(" ---- EMPLOYEE BONUS VALIDATE ----");
+		System.out.println("\u001B[36m ---- EMPLOYEE BONUS VALIDATE ---- \033[0m");
 		System.out.println("          Seja bem-vindo!       \n");
-		System.out.println("Insira a quantidade de funcionários que você irá cadastrar:");
-		int qtdEmployee = input.nextInt();
+		int qtdEmployee = (int) Utilities.readFloatInput(input, "Insira a quantidade de funcionários que você irá cadastrar:");
 		
-		for (int i=0; i < qtdEmployee; i++) {		
-			System.out.println("Insira o primeiro nome do " + (i+1) + "° funcionário:");
-			String firstName = inputEmployee.nextLine();
-			
-			System.out.println("Insira o sobrenome do " + (i+1) + "° funcionário:");
-			String lastName = inputEmployee.nextLine();
-			
-			System.out.println("Cadastre o salário do " + (i+1) + "° funcionário:");
-			float valueSalary = input.nextFloat();
-			
+		for (int i=0; i < qtdEmployee; i++) {			
+			String firstName = Utilities.readStringInput(input, "Insira o primeiro nome do " + (i+1) + "° funcionário:");
+			String lastName =  Utilities.readStringInput(input, "Insira o sobrenome do " + (i+1) + "° funcionário:");
+			float valueSalary = Utilities.readFloatInput(input, "Cadastre o salário do " + (i+1) + "° funcionário:");
 			System.out.println("------------------------------------");
 			
-			employeeController.registerEmployee(firstName, lastName, valueSalary);	
-			
-		}
-		
+			employeeController.registerEmployee(firstName, lastName, valueSalary);				
+		}		
 		employeeController.displayEmployeeData();
-
 	}
+
+
 
 }
