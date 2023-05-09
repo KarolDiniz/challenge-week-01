98f7d4e77eebbcc292e0e12bee9100b946151a1b
KarolDiniz <dinizkaroline923@gmail.com>
2023-05-09 15:17:54 -0300

    Reposicionando pacotes + aderindo encapsulamento

--- ChallengeWeek01/employeeBonusValidator/controller/EmployeeController.java
+++ ChallengeWeek01/employeeBonusValidator/controller/EmployeeController.java
@@ -6,15 +6,23 @@ import model.Employee;
 import model.Salary;
 
 public class EmployeeController {
-	
-	ArrayList<Employee> employees = new ArrayList<Employee>();	
-	
-	public void registerEmployee(String firstName, String lastName, float valueSalary) {		
+
+	/*
+	 * Esta classe foi desenvolvida com a finalidade de armazenar, inicializar e
+	 * exibir dados de um Employee, perante uma classe cliente que deseja acessá-
+	 * (que neste caso, é o main). Sendo assim, este controller realiza uma
+	 * intermediação entre o main e o model, garantindo uma melhor organização e
+	 * separação de responsabilidades em um projeto.
+	 */
+
+	ArrayList<Employee> employees = new ArrayList<Employee>();
+
+	public void registerEmployee(String firstName, String lastName, float valueSalary) {
 		Salary salary = new Salary(valueSalary);
 		Employee employee = new Employee(firstName, lastName, salary);
 		employees.add(employee);
 	}
-	
+
 	public void displayEmployeeData() {
 		for (Employee employee : employees) {
 			System.out.println(employee.toString());
@@ -24,6 +32,5 @@ public class EmployeeController {
 	public ArrayList<Employee> getEmployees() {
 		return employees;
 	}
-	
-	
+
 }
