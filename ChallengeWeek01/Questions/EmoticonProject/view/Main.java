9ddac0449f429f6c13dd72430cbb52934620db9c
KarolDiniz <dinizkaroline923@gmail.com>
2023-05-11 01:21:22 -0300

    Simple refractor in emoticonProject

--- ChallengeWeek01/Questions/EmoticonProject/view/Main.java
+++ ChallengeWeek01/Questions/EmoticonProject/view/Main.java
@@ -10,18 +10,18 @@ import utilities.Utilities;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         System.out.println("\u001B[36m ---- EMOTICON'S PROJECT ---- \033[0m");
         System.out.println("      Seja bem-vindo!            \n");
 
         boolean continuar = true;
         while (continuar) {
             //Invoca um método útil do pacote utilities.
-            String mensagem = Utilities.readStringInput(sc, "Insira uma mensagem aqui: ");
+            String mensagem = Utilities.readStringInput(input, "Insira uma mensagem aqui: ");
             Emoticon emoticon = new Emoticon(mensagem);
             System.out.println(emoticon.toString());
 
-            String resposta = Utilities.readStringInput(sc, "Deseja enviar outra mensagem? (S/N)");
+            String resposta = Utilities.readStringInput(input, "Deseja enviar outra mensagem? (S/N)");
             if (resposta.equals("N")) {
                 continuar = false;
             }
