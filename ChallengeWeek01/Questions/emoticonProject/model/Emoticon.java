2b4a9e9b0acfd717a863f745df987e1f9d7d02d6
KarolDiniz <dinizkaroline923@gmail.com>
2023-05-12 11:32:17 -0300

    implementando StringBuilder in toString
    Efetuando melhorias no cod

--- ChallengeWeek01/Questions/emoticonProject/model/Emoticon.java
+++ ChallengeWeek01/Questions/emoticonProject/model/Emoticon.java
@@ -1,8 +1,9 @@
 package emoticonProject.model;
-    /*
-    *Esta classe é responsável por representar um emoticon em uma mensagem de texto.
-    * Ela também contém métodos que são necessários para a implementação coesa do projeto.
-    */
+
+/*
+ *Esta classe é responsável por representar um emoticon em uma mensagem de texto.
+ * Ela também contém métodos que são necessários para a implementação coesa do projeto.
+ */
 public class Emoticon {
     private String mensagem;
     private int divertidos = 0;
@@ -17,8 +18,10 @@ public class Emoticon {
     private void counterEmoticons() {
         for (int i = 0; i < mensagem.length() - 1; i++) {
             if (mensagem.charAt(i) == ':' && mensagem.charAt(i + 1) == '-') {
-                if (i + 2 < mensagem.length() && mensagem.charAt(i + 2) == ')') { divertidos++;
-                } else if (i + 2 < mensagem.length() && mensagem.charAt(i + 2) == '(') { chateados++;
+                if (i + 2 < mensagem.length() && mensagem.charAt(i + 2) == ')') {
+                    divertidos++;
+                } else if (i + 2 < mensagem.length() && mensagem.charAt(i + 2) == '(') {
+                    chateados++;
                 }
             }
         }
@@ -26,8 +29,11 @@ public class Emoticon {
 
     @Override
     public String toString() {
-        return "\u001B[32mA mensagem: '" + mensagem + "' expressa um sentimento " + getSentimento() + ".\u001B[0m";
+        StringBuilder builder = new StringBuilder();
+        builder.append("\u001B[32mA mensagem: '").append(mensagem).append("' expressa um sentimento ").append(getSentimento()).append(".\u001B[0m");
+        return builder.toString();
     }
+
 
     //Responsável por retornar um sentimento de acordo com a quantidade de emoticons
     private String getSentimento() {
